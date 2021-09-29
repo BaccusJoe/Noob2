a0029d0049cb9a6970e6601c613772ba80d9a585
unknown <longyt@foxmail.com>
2021-09-29 18:20:44 +0800

    提交Noob

---
+++ Noob/Noob/Noob.cpp
@@ -0,0 +1,13 @@
+//求两个整数的和
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main()
+{
+	int a = 0;
+	int b = 0;
+	int sum = 0;
+	scanf("%d %d", &a, &b);
+	sum = a + b;
+	printf("sum=%d\n", sum);
+	return 0;
+}
