150d24d7a342c2ecee044cb1d33d048d3787ef9b
unknown <longyt@foxmail.com>
2021-09-29 17:33:09 +0800

    提交文件

---
+++ Project1/源.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+	int age = 10;
+	double weight = 81.2;
+
+	age = age + 1;
+	weight = weight - 10;
+	printf("%d\n", age);
+	printf("%lf\n", weight);
+	return 0;
+}
