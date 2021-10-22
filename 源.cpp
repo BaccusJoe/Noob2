fe7ad37a32364eb757afd5e25113423672129b95
BaccusJoe <91523326+BaccusJoe@users.noreply.github.com>
2021-10-22 17:15:48 +0800

    Add files via upload

--- 源.cpp
+++ 源.cpp
@@ -1,24 +1,36 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-#define MIN(x,y) (x<y?x:y)
+#include<stdio.h>
 //int main()
 //{
 //	int a = 0;
 //	int b = 0;
-//	int max = 0;
-//	int min = 0;
-//	scanf("%d %d",&a,&b);
-//	min = MIN(a, b);
-//	max = (a > b ? a : b);
-//	printf("max=%d", max);
-//	printf("min=%d", min);
+//	printf("输入一个数：");
+//	scanf("%d", &a);
+//	b = a % 2;
+//	printf("%d\n", b);
+//	if (1 == b)
+//	{
+//		printf("为奇数\n");
+//	}
+//	else
+//		printf("为偶数\n");
 //	return 0;
 //}
 int main()
 {
-	int a = 10;
-	int* p = &a;//取地址，p为指针变量,用来存放a的地址
-	printf("%p\n", &a);//打印地址
-	printf("%p\n", p);
+	int a = 0;
+	int b = 0;
+	printf("输入一个数：");
+	scanf("%d", &a);
+	b = a % 2;
+	switch (b)
+	{
+	case 1:
+		printf("该数为奇数\n");
+		break;
+	case 0:
+		printf("该数为偶数\n");
+		break;
+	}
 	return 0;
 }
